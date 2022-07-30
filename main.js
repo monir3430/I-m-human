0719cacf8aef44413df5d8ec12ecd968592fe16c
monir3430 <miamonir3430@gmail.com>
2022-07-31 00:57:09 +0600

    Randon color generat

--- main.js
+++ main.js
@@ -1,2 +1,30 @@
 const checkbox = document.getElementById("checkbox");
-// console.log(checkbox);
+const button = document.querySelector("button[type='submit']");
+// console.log(button)
+checkbox.disabled = true;
+button.disabled = true;
+
+const elements = document.querySelectorAll(".element");
+const colorSelect = document.getElementById("select-color");
+// console.log(colorSelect);
+
+
+elements.forEach(function (element){
+    const color = getRandomColor();
+    element.style.backgroundColor = color;
+    element.innerHTML = color;
+    colorSelect.innerHTML = color;
+})
+
+function getRandomColor (){
+    const letter = "0123456789ABCDEF"
+    let color = "#"
+    for (let i = 0; i < 6; i++){
+        color += letter[Math.floor(Math.random()*16)];
+    }
+    return color;
+}
+console.log(getRandomColor())
+
+
+

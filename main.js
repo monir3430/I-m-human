b53fe7437c3d15f2e00ae8551b4215b7a8baec83
monir3430 <miamonir3430@gmail.com>
2022-07-31 19:37:16 +0600

    Random color generator success

--- main.js
+++ main.js
@@ -24,7 +24,21 @@ function getRandomColor (){
     }
     return color;
 }
-console.log(getRandomColor())
+elements.forEach(function(element){
+    element.addEventListener('click',function(){
+      if(element.innerHTML === colorSelect.innerHTML){
+          checkbox.checked = true;
+        alert('You are human')
+        button.disabled = false;
+        button.classList.remove("btn-light")
+        button.classList.add("btn-success");
+      } else{
+        alert("Verify that You are Human !")
+        checkbox.checked = false;
+        location.reload(true);
+      }         
+    })
+})
 
 
 
